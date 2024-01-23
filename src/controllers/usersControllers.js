6237c0057f574bff98ab7cf587203a22554c5db9
LCDP69 <nicolas38.gros@gmail.com>
2024-01-23 22:37:57 +0100

    maj

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,82 +1,97 @@
 const users = [
+  {
+    id: 1,
+    firstname: "John",
+    lastname: "Doe",
+    email: "john.doe@example.com",
+    city: "Paris",
+    language: "English",
+  },
 
-    {
-      id: 1,
-      firstname: "John",
-      lastname: "Doe",
-      email: "john.doe@example.com",
-      city: "Paris",
-      language: "English"
-    },
+  {
+    id: 2,
+    firstname: "Valeriy",
+    lastname: "Appius",
+    email: "valeriy.appius@example.com",
+    city: "Moscow",
+    language: "Russian",
+  },
 
-    {
-        id: 2,
-        firstname: "Valeriy",
-        lastname: "Appius",
-        email: "valeriy.appius@example.com",
-        city: "Moscow",
-        language: "Russian"
-      },
+  {
+    id: 3,
+    firstname: "Ralf",
+    lastname: "Geronimo",
+    email: "ralf.geronimo@example.com",
+    city: "New York",
+    language: "Italian",
+  },
 
-      {
-        id: 3,
-        firstname: "Ralf",
-        lastname: "Geronimo",
-        email: "ralf.geronimo@example.com",
-        city: "New York",
-        language: "Italian"
-      },
+  {
+    id: 4,
+    firstname: "Maria",
+    lastname: "Iskandar",
+    email: "maria.iskandar@example.com",
+    city: "New York",
+    language: "German",
+  },
 
-      {
-        id: 4,
-        firstname: "Maria",
-        lastname: "Iskandar",
-        email: "maria.iskandar@example.com",
-        city: "New York",
-        language: "German"
-      },
+  {
+    id: 5,
+    firstname: "Jane",
+    lastname: "Doe",
+    email: "jane.doe@example.com",
+    city: "London",
+    language: "English",
+  },
 
-      {
-        id: 5,
-        firstname: "Jane",
-        lastname: "Doe",
-        email: "jane.doe@example.com",
-        city: "London",
-        language: "English"
-      },
-
-      {
-        id: 6,
-        firstname: "Johanna",
-        lastname: "Martino",
-        email: "johanna.martino@example.com",
-        city: "Milan",
-        language: "Spanish"
-      },
-
-  ];
+  {
+    id: 6,
+    firstname: "Johanna",
+    lastname: "Martino",
+    email: "johanna.martino@example.com",
+    city: "Milan",
+    language: "Spanish",
+  },
+];
 
 const database = require("../../database");
 
 const getUsers = (req, res) => {
-    database
-    .query("SELECT * FROM users")
+  let sql = "Select * from users";
+  const sqlValues = [];
+
+  if (req.query.language != null) {
+    sql += " where language = ?";
+    sqlValues.push(req.query.language);
+  }
+
+  if (req.query.city != null) {
+    sql += " where city = ?";
+    sqlValues.push(req.query.city);
+  }
+  else if (req.query.city !=null) {
+    sql += " where city = ?";
+    sqlValues.push(req.query.city);
+  }
+
+  database
+    .query(sql, sqlValues)
     .then(([users]) => {
       res.json(users);
     })
     .catch((err) => {
       console.error(err);
-      res.sendStatus(500)
-    })
-  };
-  
-  const getUsersById = (req, res) => {
-    const id = parseInt(req.params.id);
-  
-    database
+      res.status(500).send("Error retrieving data from database");
+    });
+};
+
+const getUsersById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
     .query("SELECT * FROM users WHERE id = ?", [id])
     .then(([users]) => {
-      if (users[0] !=null) {
+      if (users[0] != null) {
         res.json(users[0]);
       } else {
         res.sendStatus(404);
@@ -86,24 +101,23 @@ const getUsers = (req, res) => {
       console.error(err);
       res.sendStatus(500);
     });
-  };
+};
 
 const postUsers = (req, res) => {
-   const { firstname, lastname, email, city, language } = req.body;
+  const { firstname, lastname, email, city, language } = req.body;
 
-   database
-   .query(
-    "INSERT INTO users(firstname, lastname, email, city, language) VALUES  (?, ?, ?, ?, ?)",
-   [firstname, lastname, email, city, language]
-   )
-   .then(([result]) => {
-    res.status(201).send({ id: result.
-    insertId });
-   })
-   .catch((err) => {
-    console.error(err);
-    res.sendStatus(500);
-   });
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES  (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.status(201).send({ id: result.insertId });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
 const updateUser = (req, res) => {
@@ -111,45 +125,45 @@ const updateUser = (req, res) => {
   const { firstname, lastname, email, city, language } = req.body;
 
   database
-  .query(
-    "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
-    [firstname, lastname, email, city, language, id]
-  )
-  .then(([result]) => {
-    if (result.affectedRows === 0) {
-      res.sendStatus(404);
-    } else {
-      res.sendStatus(204);
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-    res.sendStatus(500);
-  });
+    .query(
+      "update users set firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?",
+      [firstname, lastname, email, city, language, id]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
 const deleteUser = (req, res) => {
   const id = parseInt(req.params.id);
 
   database
-  .query("delete from users where id = ?", [id])
-  .then(([result]) => {
-    if (result.affectedRows === 0) {
-      res.sendStatus(404);
-    } else {
-      res.sendStatus(204);
-    }
-  })
-  .catch((err) => {
-    console.error(err);
-    res.sendStatus(500);
-  });
+    .query("delete from users where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
 };
 
-  module.exports = {
-    getUsers,
-    getUsersById,
-    postUsers,
-    updateUser,
-    deleteUser,
-  };
+module.exports = {
+  getUsers,
+  getUsersById,
+  postUsers,
+  updateUser,
+  deleteUser,
+};
