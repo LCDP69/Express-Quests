cdef57e7ba1a7cd4218eff099152a50d9a9f918d
LCDP69 <nicolas38.gros@gmail.com>
2024-01-23 20:22:38 +0100

    maj

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
+const express = require("express")
 const app = require("./src/app");
 
 const port = process.env.APP_PORT;
-
+app.use(express.json())
 app
   .listen(port, () => {
     console.log(`Server is listening on ${port}`);
